6aff90e774ed86cf7df60934bf3494dd41c6d7d3
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-05-21 13:20:50 +0700

    Add lowerbound, upperbound, equalRange.

--- Search.hpp
+++ Search.hpp
@@ -11,7 +11,7 @@
 - Range: [left, right].
 - Time complexity: O(n).
 - Space complexity: O(1).
-- Return: index of the first occurrence of value in array (if found), otherwise return -1. */
+- Return: index of the first occurrence of value in array (if found); otherwise, return -1. */
 
 template <typename T>
 int linearSearch(T arr[], int left, int right, const T &value)
@@ -37,7 +37,7 @@ int linearSearch(T arr[], int sizeArr, const T &value)
 - Range: [left, right].
 - Time complexity: O(log n).
 - Space complexity: O(1) (loop), O(log n) (recursion).
-- Return: index of value in array (if found), otherwise return -1. */
+- Return: index of value in array (if found); otherwise, return -1. */
 
 // Loop version.
 
@@ -92,7 +92,7 @@ int binarySearchRecursion(T arr[], int sizeArr, const T &value, const Comp &cmp 
 - Range: [left, right].
 - Time complexity: O(log n).
 - Space complexity: O(1) (loop), O(log n) (recursion).
-- Return: index of the first occurrence of value in array (if found), otherwise return -1. */
+- Return: index of the first occurrence of value in array (if found); otherwise, return -1. */
 
 // Loop version.
 
@@ -105,7 +105,7 @@ int binarySearchFirst(T arr[], int left, int right, const T &value, const Comp &
         int mid = left + (right - left) / 2;
         if (arr[mid] == value)
         {
-            index = mid;     // lưu vị trí tìm thấy.
+            index = mid;     // lưu vị trí hợp lệ hiện tại.
             right = mid - 1; // tiếp tục tìm bên trái.
         }
         else if (cmp(arr[mid], value))
@@ -154,7 +154,7 @@ int binarySearchFirstRecursion(T arr[], int sizeArr, const T &value, const Comp 
 - Range: [left, right].
 - Time complexity: O(log n).
 - Space complexity: O(1) (loop), O(log n) (recursion).
-- Return: index of the last occurrence of value in array (if found), otherwise return -1. */
+- Return: index of the last occurrence of value in array (if found); otherwise, return -1. */
 
 // Loop version.
 
@@ -167,7 +167,7 @@ int binarySearchLast(T arr[], int left, int right, const T &value, const Comp &c
         int mid = left + (right - left) / 2;
         if (arr[mid] == value)
         {
-            index = mid;    // lưu vị trí tìm thấy.
+            index = mid;    // lưu vị trí hợp lệ hiện tại.
             left = mid + 1; // tiếp tục tìm bên phải.
         }
         else if (cmp(arr[mid], value))
@@ -207,4 +207,154 @@ template <typename T, typename Comp = std::less<T>>
 int binarySearchLastRecursion(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
 {
     return binarySearchLastRecursion(arr, 0, sizeArr - 1, value, cmp);
+}
+
+/* Lower_bound(value):
+- Vietnamese: Tìm kiếm nhị phân vị trí đầu tiên KHÔNG đứng trước value,
+tức là: "key !< value" <=> "key >= value".
+- Condition: array must be sorted according to comparator
+(mảng được sắp xếp, hay mảng là một dãy đơn điệu theo comparator).
+- Range: [left, right].
+- Time complexity: O(log n).
+- Space complexity: O(1) (loop), O(log n) (recursion).
+- Return:
++ Index of the first element that is NOT less than value according to comparator (if found).
++ Otherwise, return right + 1. */
+
+// Loop version.
+
+template <typename T, typename Comp = std::less<T>>
+int lowerBound(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
+{
+    int index = right + 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (!cmp(arr[mid], value))
+        {
+            index = mid;     // lưu vị trí hợp lệ hiện tại.
+            right = mid - 1; // tiếp tục tìm bên trái.
+        }
+        else // cmp(arr[mid], value)
+            left = mid + 1;
+    }
+    return index;
+}
+
+template <typename T, typename Comp = std::less<T>>
+int lowerBound(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
+{
+    return lowerBound(arr, 0, sizeArr - 1, value, cmp);
+}
+
+// Recursion version.
+
+template <typename T, typename Comp = std::less<T>>
+int lowerBoundRecursion(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
+{
+    if (left > right)
+        return left;
+    int mid = left + (right - left) / 2;
+    if (!cmp(arr[mid], value))
+        return lowerBoundRecursion(arr, left, mid - 1, value, cmp);
+    else // cmp(arr[mid], value)
+        return lowerBoundRecursion(arr, mid + 1, right, value, cmp);
+}
+
+template <typename T, typename Comp = std::less<T>>
+int lowerBoundRecursion(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
+{
+    return lowerBoundRecursion(arr, 0, sizeArr - 1, value, cmp);
+}
+
+/* Upper_bound(value):
+- Vietnamese: Tìm kiếm nhị phân vị trí đầu tiên value đứng trước nó,
+tức là: "value < key" <=> "key > value".
+- Condition: array must be sorted according to comparator
+(mảng được sắp xếp, hay mảng là một dãy đơn điệu theo comparator).
+- Range: [left, right].
+- Time complexity: O(log n).
+- Space complexity: O(1) (loop), O(log n) (recursion).
+- Return:
++ Index of the first element greater than value according to comparator (if found).
++ Otherwise, return right + 1. */
+
+// Loop version.
+
+template <typename T, typename Comp = std::less<T>>
+int upperBound(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
+{
+    int index = right + 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (cmp(value, arr[mid]))
+        {
+            index = mid;     // lưu vị trí hợp lệ hiện tại.
+            right = mid - 1; // tiếp tục tìm bên trái.
+        }
+        else // !cmp(value, arr[mid])
+            left = mid + 1;
+    }
+    return index;
+}
+
+template <typename T, typename Comp = std::less<T>>
+int upperBound(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
+{
+    return upperBound(arr, 0, sizeArr - 1, value, cmp);
+}
+
+// Recursion version.
+
+template <typename T, typename Comp = std::less<T>>
+int upperBoundRecursion(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
+{
+    if (left > right)
+        return left;
+    int mid = left + (right - left) / 2;
+    if (cmp(value, arr[mid]))
+        return upperBoundRecursion(arr, left, mid - 1, value, cmp);
+    else // !cmp(value, arr[mid])
+        return upperBoundRecursion(arr, mid + 1, right, value, cmp);
+}
+
+template <typename T, typename Comp = std::less<T>>
+int upperBoundRecursion(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
+{
+    return upperBoundRecursion(arr, 0, sizeArr - 1, value, cmp);
+}
+
+/* Equal_range(value):
+- Vietnamese: Tìm kiếm nhị phân khoảng xuất hiện của value trong mảng.
+- Condition: array must be sorted according to comparator
+(mảng được sắp xếp, hay mảng là một dãy đơn điệu theo comparator).
+- Range: [left, right].
+- Time complexity: O(log n).
+- Space complexity: O(1).
+- Return:
++ A pair of indices [first, last]:
+    . first: index of the first occurrence of value in array.
+    . last: index of the last occurrence of value in array.
++ If value does not exist in array, return [-1, -1]. */
+
+struct Range
+{
+    int first;
+    int last;
+};
+
+template <typename T, typename Comp = std::less<T>>
+Range equalRange(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
+{
+    Range ans;
+    ans.first = binarySearchFirst(arr, left, right, value, cmp);
+    ans.last = binarySearchLast(arr, left, right, value, cmp);
+    return ans;
+}
+
+template <typename T, typename Comp = std::less<T>>
+Range equalRange(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
+{
+    return equalRange(arr, 0, sizeArr - 1, value, cmp);
 }
