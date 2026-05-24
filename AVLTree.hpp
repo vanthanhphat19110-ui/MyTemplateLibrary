60a90935b3ffbd2e2ed3b67522a580f951bba605
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-05-24 19:22:11 +0700

    Add init, height, update.

--- AVLTree.hpp
+++ AVLTree.hpp
@@ -14,3 +14,37 @@ struct AVLTree
 {
     AVLNode<T> *root;
 };
+
+template <typename T>
+void init(AVLTree<T> &tree)
+{
+    tree.root = nullptr;
+}
+
+template <typename T>
+int height(const AVLNode<T> *&root)
+{
+    if (root == nullptr)
+        return 0;
+    return root->height;
+}
+
+template <typename T>
+int height(const AVLTree<T> &tree)
+{
+    return height(tree.root);
+}
+
+template <typename T>
+void update(AVLNode<T> *&root)
+{
+    if (root == nullptr)
+        return;
+    root->height = 1 + max(height(root->left), height(root->right));
+}
+
+template <typename T>
+void update(AVLTree<T> &tree)
+{
+    update(tree.root);
+}
