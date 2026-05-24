92a4a12b1641e6c3e894334bdeab6f97ad0fd500
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-05-24 16:27:53 +0700

    Finished Stack.hpp

--- Stack.hpp
+++ Stack.hpp
@@ -1,1 +1,73 @@
 /* Thư viện Stack. */
+
+template <typename T>
+struct StackNode
+{
+    T data;
+    StackNode<T> *next;
+    StackNode(const T &value) : data(value), next(nullptr) {};
+};
+
+template <typename T>
+struct Stack
+{
+    StackNode<T> *top;
+    int size;
+};
+
+template <typename T>
+void init(Stack<T> &st)
+{
+    st.top = nullptr;
+    st.size = 0;
+}
+
+template <typename T>
+bool isEmpty(const Stack<T> &st)
+{
+    return st.top == nullptr;
+}
+
+template <typename T>
+int getSize(const Stack<T> &st)
+{
+    return st.size;
+}
+
+template <typename T>
+const T &top(const Stack<T> &st)
+{
+    return st.top->data;
+}
+
+template <typename T>
+void push(Stack<T> &st, const T &value)
+{
+    StackNode<T> *p = new StackNode<T>(value);
+    p->next = st.top;
+    st.top = p;
+    st.size++;
+}
+
+template <typename T>
+void pop(Stack<T> &st)
+{
+    if (st.top == nullptr)
+        return;
+    StackNode<T> *temp = st.top;
+    st.top = st.top->next;
+    delete temp;
+    st.size--;
+}
+
+template <typename T>
+void clear(Stack<T> &st)
+{
+    while (st.top != nullptr)
+    {
+        StackNode<T> *temp = st.top;
+        st.top = st.top->next;
+        delete temp;
+    }
+    st.size = 0;
+}
