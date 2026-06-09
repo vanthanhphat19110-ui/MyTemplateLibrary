c6e8f298c0ed735887c5d8e65107258ccfbae840
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-06-09 10:09:55 +0700

    Add a defensive comment for function front() in Queue.hpp

--- Queue.hpp
+++ Queue.hpp
@@ -36,7 +36,7 @@ int size(const Queue<T> &q)
     return q.size;
 }
 
-/* Cần đảm bảo Queue khác rỗng trước khi gọi top(). */
+/* Cần đảm bảo Queue khác rỗng trước khi gọi front(). */
 template <typename T>
 const T &front(const Queue<T> &q)
 {
