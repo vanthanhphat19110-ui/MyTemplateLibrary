37fdc5115a7c7dfdbbcac9dec77999cb964f449f
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-06-11 19:22:19 +0700

    Finished Sort.hpp

--- Sort.hpp
+++ Sort.hpp
@@ -4,7 +4,7 @@
 02. T& --> không copy (&) và cho phép thay đổi (no const). */
 // Chú ý điều kiện vòng lặp.
 
-#include <functional> // chứa std::less<> mặc định.
+#include <functional>
 
 template <typename T>
 void mySwap(T &a, T &b)
