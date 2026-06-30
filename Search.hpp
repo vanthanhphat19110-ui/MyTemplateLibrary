b832dcd1ae7682fdb9f85fa8e8179afcfb0d8389
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-06-30 08:06:49 +0700

    Edit comments.

--- Search.hpp
+++ Search.hpp
@@ -2,8 +2,7 @@
 
 /* Chú ý kiểu dữ liệu:
 01. const T& --> không copy (&) và không cho phép thay đổi dữ liệu (const).
-02. T& --> không copy (&) và cho phép thay đổi dữ liệu (no const).
-*/
+02. T& --> không copy (&) và cho phép thay đổi dữ liệu (no const). */
 
 /*
 - Chú ý điều kiện vòng lặp của các hàm liên quan đến binary search.
